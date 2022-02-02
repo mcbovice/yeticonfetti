348aa7a03d05a5c88cf408ae110ee210340231f3
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-02-02 08:52:12 -0500

    add basic structure to repository

---
+++ Client Advanced/src/core/Lib/Repository/Repository.js
@@ -0,0 +1,13 @@
+class Repository{
+    constructor(tableName = ""){
+        this.tableName = tableName;
+
+        this.#_init();
+    }
+
+    #_init(){
+
+    }
+}
+
+module.exports = Repository;
---
+++ Client Advanced/src/core/Repository/ServerRepository.js
@@ -0,0 +1,9 @@
+var Repository = require("../Lib/Repository/Repository");
+
+class ServerRepository extends Repository{
+    constructor(){
+        
+    }
+}
+
+module.exports = ServerRepository;
