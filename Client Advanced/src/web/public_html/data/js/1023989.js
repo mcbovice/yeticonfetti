371903115d17b11c9303da0433b47688db5f1dfc
mcbovice <mcbovice@gmail.com>
2022-02-23 09:27:29 -0500

    Update 1023989.js

--- Client Advanced/src/web/public_html/data/js/1023989.js
+++ Client Advanced/src/web/public_html/data/js/1023989.js
@@ -38535,8 +38535,8 @@
         this.position.x = this.pos_x - d;
         this.position.y = this.pos_y - e;
         this.scale.x = this.is_flip ? -1 : 1;
-        this.is_half_size && (this.scale.x *= 1,
-        this.scale.y = 1);
+        this.is_half_size && (this.scale.x *= 0.35,
+        this.scale.y = 0.35);
         this.holdSpotX = f;
         this.holdSpotY = h;
         if (this.change_z_when_special && (c = this.is_special ? this.z_index + this.change_z_when_special : this.z_index,
